f1f3b68dbd79e203ad7c79fb88bf7d38b64bfc88
Guillaume THIVOLET <guillaume.thivolet@free.fr>
2017-10-06 17:04:44 +0200

    first working prototype

--- main.py
+++ main.py
@@ -6,53 +6,67 @@ class TextLineNumbers(tk.Canvas):
         tk.Canvas.__init__(self, *args, **kwargs)
         self.textwidget = None
         self.lastline =  1
+        self.line_at_beginning = 0
 
     def attach(self, text_widget):
         self.textwidget = text_widget
-        log = open("test.txt", "r")
-
-        texts = []
-
-        for c,l in enumerate(log):
-            if len(l.split("|")) >= 2:
-                self.textwidget.insert("%d.0" % (c+1), l.split("|")[1])
-            else:
-                print ("error")
+        try:
+            log = open("test.txt", "r")
+            for c,l in enumerate(log):
+                if len(l.split("|")) >= 2:
+                    self.textwidget.insert("%d.0" % (c+1), l.split("|")[1])
+                else:
+                    print ("error")
+            self.line_at_beginning = c + 1
+        except FileNotFoundError:
+            pass
 
     def redraw(self, *args):
         '''redraw line numbers'''
         self.delete("all")
 
         i = self.textwidget.index("@0,0")
-        log = open("test.txt", "r")
-
-        texts = []
         hours = []
+        empty_lines = 0
+        count = 0
+        try:
+            log = open("test.txt", "r")
+            for c,l in enumerate(log):
+                count  = c + 1
+                if len(l.split("|")) >= 2:
+                    hours.append(l.split("|")[0])
+                else:
+                    empty_lines = empty_lines + 1
+                    pass
+            log.close()
+        except FileNotFoundError:
+            pass
 
-        for c,l in enumerate(log):
-            if len(l.split("|")) >= 2:
-                hours.append(l.split("|")[0])
-            else:
-                pass
-
-        lastlinenumber = c + 1
-        self.lastline = c + 1
+        self.lastline = count - empty_lines  + 1
 
         while True :
             dline= self.textwidget.dlineinfo(i)
             if dline is None: break
             y = dline[1]
             linenum = str(i).split(".")[0]
+            line_text = self.textwidget.get("%d.0" % int(linenum), "%d.end" % int(linenum))
 
-            if (int(linenum) - 1) >= self.lastline:
-                log = open("test.txt", "a")
-                heure = strftime("%H:%M:%S", time.localtime())
-                log.write("\n" + heure +  " | nouvelle heure")
-                hours.append(heure)
-                log.close()
+            if (int(linenum)) > self.lastline:
+                if self.line_at_beginning < (int(linenum) + 2):
+                    last_line = self.textwidget.get("%d.0" % (int(linenum)-1), "%d.end" % (int(linenum)-1))
+                    log = open("test.txt", "a")
+                    log.write(heure +  "|" + last_line + "\n")
                 self.lastline = int(linenum)
+
+            if (len(line_text) >= 1) and self.lastline == int(linenum):
+                heure = strftime("%H:%M:%S", time.localtime())
+                if len(hours) < self.lastline:
+                     hours.append(heure)
+
             print(linenum + " and last: " + str(self.lastline))
-            if (int(linenum) < self.lastline):
+            print(hours)
+
+            if (int(linenum) <= self.lastline and len(line_text) >= 1):
                 self.create_text(2,y,anchor="nw", text=(linenum+" "+hours[int(linenum)-1]))
             else:
                 self.create_text(2,y,anchor="nw", text=linenum)
